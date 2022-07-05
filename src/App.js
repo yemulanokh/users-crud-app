f33c074a0ac4d226d31bad8e15dfd5b17eea3bc8
yemulanokh <anokh@shrisaitechnology.com>
2022-07-05 17:00:00 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { AddUsers } from './components/AddUsers';
 import { GetUsers } from './components/GetUsers';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div className="App">
-     <AddUsers/>
-     <GetUsers/>
-     
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<GetUsers/>}/>
+    <Route path="/post" element={<AddUsers/>} />
+     </Routes>
+     </BrowserRouter>
   );
 }
 
